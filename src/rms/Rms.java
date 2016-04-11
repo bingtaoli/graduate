6100fd7f481753758b67a0a5ea18a5f791a04fcb
bingtaoli <1464738780@qq.com>
2016-04-11 21:40:47 +0800

    remove useless import

--- src/rms/Rms.java
+++ src/rms/Rms.java
@@ -7,8 +7,6 @@ import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.jobcontrol.ControlledJob;
-import org.apache.hadoop.mapreduce.lib.jobcontrol.JobControl;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
