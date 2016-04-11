00b9242ed10c553cec7cea1a0d180ae68787a866
bingtaoli <1464738780@qq.com>
2016-04-11 23:08:05 +0800

    add MyPrinter to replace System.out.print

---
+++ src/utils/MP.java
@@ -0,0 +1,14 @@
+package utils;
+
+/**
+ * 
+ * @author libingtao
+ * for remove ugly System.out :) 
+ */
+public class MP {
+
+	public static void println(Object obj){
+		System.out.println(obj);
+	}
+	
+}
