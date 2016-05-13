986a47339bc26914b5e7a381b2fc15f9fa01f67f
bingtaoli <1464738780@qq.com>
2016-05-13 21:27:30 +0800

    Wrap REngine init into this class

---
+++ src/utils/MyR.java
@@ -0,0 +1,31 @@
+package utils;
+
+import org.rosuda.JRI.Rengine;
+
+public class MyR {
+
+	public static Rengine getREngine(){
+		/**
+		 * R engine test
+		 */
+		// just making sure we have the right version of everything
+		if (!Rengine.versionCheck()) {
+		    System.err.println("** Version mismatch - Java files don't match library version.");
+		    System.exit(1);
+		}
+        System.out.println("Creating Rengine (with arguments)");
+		// 1) we pass the arguments from the command line
+		// 2) we won't use the main loop at first, we'll start it later
+		//    (that's the "false" as second argument)
+		// 3) the callbacks are implemented by the TextConsole class above
+		Rengine re=new Rengine(new String[] { "--vanilla" }, false, null);
+        System.out.println("Rengine created, waiting for R");
+		// the engine creates R is a new thread, so we should wait until it's ready
+        if (!re.waitForR()) {
+            System.out.println("Cannot load R");
+            return null;
+        }
+        return re;
+	}
+	
+}
