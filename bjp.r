eda610043c7024daac2016968bf0804a130fdd18
bingtaoli <1464738780@qq.com>
2016-05-13 20:36:56 +0800

    bjp.r中记录了根据amp和freq向量计算边际谱的方法

---
+++ bjp.r
@@ -0,0 +1,37 @@
+library(EMD)
+
+ampfreq <- read.csv('/Users/libingtao/graduate/emdresult.txt', header=FALSE)
+amplitude <- ampfreq[,2]
+insfreq <- ampfreq[,3]
+#现在的频率是0~1之间，可能是经过了归一化
+#首先把频率放大为0~400
+bigger <- 400 / max(insfreq)
+bigger <- floor(bigger) #向下取整
+for (i in 1:3000){
+    insfreq[i] = floor(insfreq[i] * bigger)
+}
+#构建一个涵盖两个矩阵信息的矩阵
+toimageResult <- matrix(data=0, nrow=400, ncol=3000)
+for (i in 1:3000){
+    freq <- insfreq[i]
+    toimageResult[freq, i] = amplitude[i]
+    cat(freq, " ", i, " is ", amplitude[i], "\n")
+}
+#求sum
+bjp <- array(1:400)
+for (i in 1:400){
+    sum <- sum(toimageResult[i,])
+    bjp[i] = sum
+}
+
+
+
+xx <- 1:400
+
+plot(xx, bjp, type='l')
+
+library(signal)
+
+hir <- interp1(xx, bjp, 221)
+hor <- interp1(xx, bjp, 168)
+hbr <- interp1(xx, bjp, 215.48)
