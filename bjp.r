b22d6a2ecefcfbee579ed98e9986e2f7c19dba52
bingtaoli <1464738780@qq.com>
2016-05-13 21:03:47 +0800

    Fix NA bug

--- bjp.r
+++ bjp.r
@@ -1,36 +1,37 @@
 library(EMD)
+library(signal)
 
 ampfreq <- read.csv('/Users/libingtao/graduate/emdresult.txt', header=FALSE)
-amplitude <- ampfreq[,2]
-insfreq <- ampfreq[,3]
+amplitude <- ampfreq[,1]
+insfreq <- ampfreq[,2]
+originDataLength <- length(amplitude)
 #现在的频率是0~1之间，可能是经过了归一化
 #首先把频率放大为0~400
-bigger <- 400 / max(insfreq)
+bigger <- 400 / max(insfreq, na.rm=TRUE) #去除NA的干扰
 bigger <- floor(bigger) #向下取整
-for (i in 1:3000){
-    insfreq[i] = floor(insfreq[i] * bigger)
+for (i in 1:originDataLength){
+	if (is.na(insfreq[i])) {
+		insfreq[i] = 0
+	}
+	insfreq[i] = floor(insfreq[i] * bigger)
 }
 #构建一个涵盖两个矩阵信息的矩阵
-toimageResult <- matrix(data=0, nrow=400, ncol=3000)
-for (i in 1:3000){
+toimageResult <- matrix(data=0, nrow=400, ncol=originDataLength)
+for (i in 1:originDataLength){
     freq <- insfreq[i]
     toimageResult[freq, i] = amplitude[i]
-    cat(freq, " ", i, " is ", amplitude[i], "\n")
+    #cat(freq, " ", i, " is ", amplitude[i], "\n")
 }
 #求sum
 bjp <- array(1:400)
 for (i in 1:400){
-    sum <- sum(toimageResult[i,])
+    sum <- sum(toimageResult[i,], na.rm=TRUE)
     bjp[i] = sum
 }
-
-
 
 xx <- 1:400
 
 plot(xx, bjp, type='l')
-
-library(signal)
 
 hir <- interp1(xx, bjp, 221)
 hor <- interp1(xx, bjp, 168)
